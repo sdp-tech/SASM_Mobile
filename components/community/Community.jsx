ab69d44987aef23fe5193f97e3027d24e6c3f1f5
Sejong Kim <sepaper@naver.com>
2023-02-17 00:13:26 +0900

    refactor : improve component structure organization
    Signed-off-by: Sejong Kim <sepaper@naver.com>

---
+++ components/community/Community.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Text, SafeAreaView } from "react-native";
+
+const CommunityScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView>
+            <Text>Community</Text>
+        </SafeAreaView>
+    )
+}
+
+export default CommunityScreen;
---
+++ components/story/Story.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Text, SafeAreaView } from "react-native";
+
+const StoryScreen = ({ navigation }) => {
+    return (
+        <SafeAreaView>
+            <Text>Story</Text>
+        </SafeAreaView>
+    )
+}
+
+export default StoryScreen;
