19fe04dfd12babab936925e398bc0fd6fa8feb57
SJ-Kwak <seojin7@ewhain.net>
2023-03-13 21:47:57 +0900

    sdp-tech#43 feat: add story list, detail screen

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Text, SafeAreaView, View, Button, Dimensions } from "react-native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import StoryListPage from '../components/story/StoryListPage';
+import StoryDetailPage from '../components/story/StoryDetailPage';
+
+const Stack = createNativeStackNavigator();
 
 const StoryScreen = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <Text>story</Text>
-    </SafeAreaView>
+    <Stack.Navigator 
+      screenOptions = {() => ({
+        headerShown: true,
+      })}
+    >
+      <Stack.Screen name = "StoryList" component = {StoryListPage} />
+      <Stack.Screen name = "StoryDetail" component = {StoryDetailPage} />
+    </Stack.Navigator>
   )
 }
 
