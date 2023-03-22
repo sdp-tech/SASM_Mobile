cd553b32611e3fecb3dccb121d93d6d61a3adb03
Seojin Kwak <81500249+SJ-Kwak@users.noreply.github.com>
2023-03-22 12:52:15 +0900

    #43 feat : add story search

--- src/components/story/StoryListPage.jsx
+++ src/components/story/StoryListPage.jsx
@@ -1,59 +1,13 @@
 import { useState, useEffect, useNavigate } from 'react';
 import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-//import AsyncStorage from '@react-native-async-storage/async-storage';
 import styled from 'styled-components/native';
 import SearchBar from '../../common/SearchBar';
 import Pagination from '../../common/Pagination';
-import { useCookies } from "react-cookie";
-//import { useNavigate } from "react-router-dom";
 import Loading from "../../common/Loading";
 import StoryList from './components/StoryList';
 import { Request } from '../../common/requests';
 import { useNavigation } from '@react-navigation/native';
-//import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const Section = styled.View`
-  box-sizing: border-box;
-  position: relative;
-  height: calc(100vh - 114px)%;
-  min-height: 100%;
-  width: 100%;
-  grid-area: story;
-  display: flex;
-  flex-direction: column;
-`;
-const SearchBarSection = styled.View`
-  box-sizing: border-box;
-  position: relative;
-  height: 8vh%;
-  width: 100%;
-  display: flex;
-  margin-top: 0.1%;
-  flex-direction: row;
-  grid-area: story;
-  align-items: center;
-  justify-content: center;
-  @media screen and (max-width: 768px) {
-    margin-top: 3vh;
-    flex-direction: column;
-    height: 10vh;
-    justify-content: space-between;
-    align-items: center;
-  }
-`;
-const StoryListSection = styled.View`
-  box-sizing: border-box;
-  position: relative;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  grid-area: story;
-  scrollbar-height: thin;
-  overflow: scroll;
-  @media screen and (max-width: 768px) {
-  }
-`;
 const FooterSection = styled.View`
   display: flex;
   flex-direction: row;
@@ -64,19 +18,7 @@ const FooterSection = styled.View`
   z-index: 20;
   justify-content: center;
   align-items: center;
-  background-color: #EEEEEE;
-`;
-const SearchFilterBar = styled.View`
-  box-sizing: border-box;
-  width: 35%;
-  @media screen and (max-width: 768px) {
-    width: 80%;
-    height: 4vh;
-  }
-  height: 50%;
-  display: flex;
-  background: #FFFFFF;
-  border-radius: 56px;
+  background-color: #FFFFFF;
 `;
 
 const ToggleButton = ({onPress, text, color, textColor }) => {
@@ -105,38 +47,21 @@ const ToggleButton = ({onPress, text, color, textColor }) => {
     )
 }
 
-const StoryListPage = () => {
+const StoryListPage = ({ navigation, route }) => {
     const [item, setItem] = useState([]);
-    const [searchToggle, setSearchToggle] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [toggleOpen, setToggleOpen] = useState(false);
     const [orderList, setOrderList] = useState(true);
     const [page, setPage] = useState(1);
     const [pageCount, setPageCount] = useState([]);
     const [limit, setLimit] = useState(4);
     const [search, setSearch] = useState('');
-    
-    //const token = AsyncStorage.getItem("accessTK");
     const request = new Request();
-
-    const handleOrderToggle = () => {
-        setOrderList(!orderList);
-    }
-
-    const onChangeSearch = (e) => {
-        e.preventDefault();
-        setSearchToggle(e.target.value);
-    }
     
     useEffect(() => {
         handleSearchToggle();
-    }, [page, orderList])
+    }, [page, search, orderList])
 
-    const handleSearchToggle = async (e) => {
-        if (e) {
-            e.preventDefault();
-        }
-        setSearchToggle(true);
+    const handleSearchToggle = async () => {
         setLoading(true);
 
         let newPage;
@@ -146,30 +71,29 @@ const StoryListPage = () => {
             newPage = page;
         }
 
-        // let headerValue;
-        // if (token === null || token === undefined) {
-        //     headerValue = `No Auth`;
-        // } else {
-        //     headerValue = `Bearer ${token}`;
-        // }
-
         let searched;
         if (search === null || search === "") {
             searched = null;
         } else {
             searched = search;
         }
-
+        
         const response = await request.get('/stories/story_search/', {
             page: newPage,
             search: searched,
-            order: orderList.toString(),
+            latest: orderList
         }, null);
-
         setItem(response.data.data.results);
-        console.log("item => ", item)
         setPageCount(response.data.data.count);
         setLoading(false);
+    }
+
+    const onEndReached = () => {
+        if(loading)
+            return;
+        else {
+            setPage(page + 1);
+        }
     }
 
     return (
@@ -196,7 +120,7 @@ const StoryListPage = () => {
                     }}>
                         공간에 대한 깊은 이야기
                     </Text>
-                    <SearchBar />
+                    <SearchBar setSearch = {setSearch} />
                     <View style = {{
                         backgroundColor: '#EEEEEE',
                         borderRadius: 12,
@@ -232,6 +156,8 @@ const StoryListPage = () => {
                     </View>
                     <StoryList 
                         info = {item}
+                        //onEndReached = {onEndReached}
+                        //loading = {loading}
                     />
                     <FooterSection>
                         <Pagination
