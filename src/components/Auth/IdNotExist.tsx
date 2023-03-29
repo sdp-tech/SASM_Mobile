20636a9ba26a0401223b55bd06c9e1c0cf023067
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-03-29 21:24:55 +0900

    #69 feat : navigation address change

--- src/components/Auth/IdNotExist.tsx
+++ src/components/Auth/IdNotExist.tsx
@@ -1,10 +1,10 @@
-import { StackScreenProps } from '@react-navigation/stack';
+import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { findScreenProps } from './FindIDPW';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
-import { MyPageNavProps } from '../../pages/MyPage';
+import { MyPageProps } from '../../pages/MyPage';
 
 const StyledButton = styled.TouchableOpacity`
   border: 1px #000000 solid;
@@ -14,11 +14,11 @@ const StyledButton = styled.TouchableOpacity`
 `
 
 export default function IdNotExist({ navigation, route }: StackScreenProps<findScreenProps, 'idNotExist'>): JSX.Element {
-  const navigatetToMyPage = useNavigation<MyPageNavProps>();
+  const navigatetToMyPage = useNavigation<StackNavigationProp<MyPageProps>>();
   return (
     <View>
       <Text>{route.params.email}는 SASM에 등록되지 않은 이메일입니다.</Text>
-      <StyledButton onPress={()=>{navigatetToMyPage.navigate('join')}}><Text>회원가입 하기</Text></StyledButton>
+      <StyledButton onPress={()=>{navigatetToMyPage.navigate('register')}}><Text>회원가입 하기</Text></StyledButton>
       <StyledButton onPress={()=>{navigation.goBack();}}><Text>다른 아이디 확인</Text></StyledButton>
     </View>
   )
