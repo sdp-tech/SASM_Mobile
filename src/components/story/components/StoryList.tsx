87468f935998ca50359c891cda3ad748fd95022b
SJ-Kwak <seojin7@ewhain.net>
2023-03-29 23:47:03 +0900

    sdp-tech#43 feat: fix story infinite scroll

--- src/components/story/components/StoryList.tsx
+++ src/components/story/components/StoryList.tsx
@@ -1,43 +1,40 @@
+import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 import ItemCard from './ItemCard';
 
 interface StoryListProps {
     info: any;
     onEndReached: any;
-    loading: boolean;
     onRefresh: any;
     refreshing: boolean;
     navigation: any;
+    page: number;
 }
 
-const StoryList = ({ info, onEndReached, loading, onRefresh, refreshing, navigation }: StoryListProps) => {
+const StoryList = ({ info, onEndReached, onRefresh, refreshing, navigation, page }: StoryListProps) => {
     return (
         <>
-            {info.length === 0 ? (
-                <Text style = {{ marginTop: 15 }}>해당하는 스토리가 없습니다</Text>
-            ) : (
-                <FlatList
-                    data = {info}
-                    renderItem = {({item}) => (
-                        <ItemCard
-                            id = {item.id}
-                            rep_pic = {item.rep_pic}
-                            place_name = {item.place_name}
-                            title = {item.title}
-                            category = {item.category}
-                            story_like = {item.story_like}
-                            preview = {item.preview}
-                            navigation = {navigation}
-                        />
-                    )}
-                    keyExtractor = {(item, index) => String(index)}
-                    onRefresh = {onRefresh}
-                    refreshing = {refreshing}
-                    onEndReached = {onEndReached}
-                    showsVerticalScrollIndicator = {true}
-                    ListFooterComponent = {loading ? <ActivityIndicator /> : null}
-                />
-            )}
+            <FlatList
+                data = {info}
+                renderItem = {({item}) => (
+                    <ItemCard
+                        id = {item.id}
+                        rep_pic = {item.rep_pic}
+                        place_name = {item.place_name}
+                        title = {item.title}
+                        category = {item.category}
+                        story_like = {item.story_like}
+                        preview = {item.preview}
+                        navigation = {navigation}
+                    />
+                )}
+                keyExtractor = {(item, index) => String(index)}
+                onRefresh = {onRefresh}
+                refreshing = {refreshing}
+                onEndReached = {onEndReached}
+                showsVerticalScrollIndicator = {true}
+                ListEmptyComponent = {<Text style = {{ marginTop: 15}}>해당하는 스토리가 없습니다</Text>}
+            />
         </>
     )
 }
