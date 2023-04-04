0d48fa6dfb4884c60ea3620a926d00e16b0158dd
SeoJimin <jnuni004@naver.com>
2023-04-05 02:36:38 +0900

    #89 feat:mypick screen add style

--- src/common/Category.tsx
+++ src/common/Category.tsx
@@ -49,14 +49,24 @@ export const CATEGORY_LIST: ListProps[] = [
   { id: 5, data: "녹색 공간", name: "녹색공간", sourceWhite: require("../assets/img/Category/CategoryWhite5.svg") },
 ];
 
-export function MatchCategory(data: string): number {
+// export function MatchCategory(data: string): number {
+//   let index: number = 0;
+//   for (let i: number = 0; i < 6; i++) {
+//     if (CATEGORY_LIST[i].data == data) {
+//       index = i;
+//     }
+//   }
+//   return index;
+// }
+
+export function MatchCategory(data: string): string {
   let index: number = 0;
   for (let i: number = 0; i < 6; i++) {
     if (CATEGORY_LIST[i].data == data) {
       index = i;
     }
   }
-  return index;
+  return '../../../assets/img/Category/Category'+index+'.svg';
 }
 
 
--- src/components/mypick/mystory/ItemCard.jsx
+++ src/components/mypick/mystory/ItemCard.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
 import { TouchableOpacity, StyleSheet, Image, View,Button, Text } from 'react-native';
-//import Card from "@mui/material/Card";
-//import CardContent from "@mui/material/CardContent";
-//import { CardMedia } from "@mui/material";
-import HeartButton from "../../../common/Heart";
 //import { useCookies } from "react-cookie";
 import axios from "axios";
 import Loading from "../../../common/Loading";
-//import Typography from "@mui/material/Typography";
-//import { Link, useNavigate } from "react-router-dom";
 import {Request} from "../../../common/requests";
 import { MatchCategory, CATEGORY_LIST } from "../../../common/Category";
-//import { useMediaQuery } from "react-responsive";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
+import Heart from '../../../common/Heart';
 
 const styles = StyleSheet.create({
   PlacenameBox:{
@@ -52,44 +46,8 @@ const styles = StyleSheet.create({
   },
 })
 
-// const PlacenameBox = styled.View`
-//   box-sizing: border-box;
-//   display: flex;
-//   width: 100%;
-//   font-weight: 550;
-//   font-size: 1.2rem;
-//   align-items: center;
-//   color:#000000;
-//   margin-top: -2%;
-//   margin-bottom: 5%;
-//   justify-content: space-between;
-  
-// `;
-
-// // 기존에 존재하는 버튼에 재스타일
-// const Button = styled(TouchableOpacity)`
-//   background-color: #ffffff;
-//   height: 50px;
-//   font-size: 20px;
-//   font-weight: 700;
-//   border-radius: 15px;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   cursor: pointer;
-// `;
-// const LikeButton = styled(Button)({
-//   boxSizing: "border-box",
-//   border: "none",
-//   display: "flex",
-//   height: "30px",
-//   width: "30px",
-// });
 
 function ItemCard(props) {
-  // const isMobile = useMediaQuery({ query: '(max-width:768px)' });
-  // const width = isMobile ? '80vw' : '32vw';
-  // const height = isMobile ? '45vw' : '18vw';
   const [like, setLike] = useState(false);
   //const [cookies, setCookie, removeCookie] = useCookies(["name"]);
   const [loading, setLoading] = useState(true);
@@ -97,9 +55,11 @@ function ItemCard(props) {
   const request = new Request();
 
   // 좋아요 클릭 이벤트
-  const toggleLike = async (id) => {
-    const response = await request.post("/story/story_like/", { id: id }, null); 
+  const toggleLike = async () => {
+    const response = await request.post("/stories/story_like/", { id: props.story_id }, null); 
+    console.log("mystory", response);
     setLike(!like);
+
   };
 
   return (
@@ -107,13 +67,13 @@ function ItemCard(props) {
       <View>
         <Image
         source={{uri: props.rep_pic} }
-        width={100}
-        height={100}
+        width={150}
+        height={200}
         />
         
 
           <TouchableOpacity
-            onPress={() => navigation.navigate('스토리', { story_id: props.story_id })}
+            onPress={() => {navigation.navigate('스토리', { id: props.story_id }) }}
             style={{ textDecoration: 'none' }}
           >
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -130,9 +90,13 @@ function ItemCard(props) {
                   }}
                   style={{ width: 20, height: 20 }}
                 />
-                <Button style={styles.LikeButton} title='LikeButton'>
-                  <Button style={styles.HeartButton} like={!like} onClick={() => toggleLike(props.id)} title='HeartButton' />
-                </Button>
+                <View>
+                    {props.story_like === 'ok' ? (
+                        <Heart like={!like} onPress={toggleLike} />
+                    ) : (
+                        <Heart like={like} onPress={toggleLike} />
+                    )}
+                </View>
               </View>
             </View>
           </TouchableOpacity>
@@ -148,87 +112,3 @@ function ItemCard(props) {
 }
 
 export default ItemCard;
-/*
-export default function ItemCard(props) {
-  const isMobile = useMediaQuery({query: "(max-width:768px)"});
-  const width = isMobile?"80vw":"32vw";
-  const height = isMobile?"45vw":"18vw";
-  const [like, setLike] = useState(false);
-  const [cookies, setCookie, removeCookie] = useCookies(["name"]);
-  const [loading, setLoading] = useState(true);
-  //const navigate = useNavigate();
-  const request = new Request(cookies, localStorage, navigate);
-  // 좋아요 클릭 이벤트
-  const toggleLike = async (id) => {
-    const response = await request.post("/places/place_like/", { id: id }, null);
-    //console.log("response", response);
-
-    //색상 채우기
-    setLike(!like);
-  };
-
-  return (
-    <View>
-      <Card
-        sx={{
-          minWidth: width,
-          maxWidth: width,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <CardMedia
-          component="img"
-          sx={{
-            // 16: 9,
-            minHeight: height,
-            minWidth: width,
-            maxHeight: height,
-            maxWidth: width,
-            display: "flex",
-          }}
-          image={props.rep_pic}
-          alt="placeImage"
-        />
-
-        <CardContent
-          sx={{
-            minWidth: width,
-            maxWidth: width,
-            display: "flex",
-            flexFlow: "column",
-          }}
-        >
-          <Link to={`/story/${props.story_id}`} style={{ textDecoration: 'none' }}>
-            <PlacenameBox>
-              <Text>{props.place_name}</Text>
-              <View style={{display:'flex', marginRight:10}}>
-              <Image
-              source={{
-                uri: `../../../assets/img/Category/Category${MatchCategory(props.category)}.svg`,
-                cache: 'only-if-cached',
-                headers: {Pragma: 'no-cache'}
-              }}
-              onError={() => {// Load a fallback image
-                require('../../../assets/img/Category/Category0.svg');
-                }}/>
-                <LikeButton>
-                  <HeartButton like={!like} onClick={() => toggleLike(props.id)} />
-                </LikeButton>
-              </View>
-            </PlacenameBox>
-          </Link>
-          <View style={{borderBottom:"2px #44ADF7 solid" ,paddingBottom:10}}>
-            {props.title}
-          </View>
-          <View style={{paddingTop:10, fontSize:"0.8rem"}}>
-            {props.preview}
-          </View>
-        </CardContent>
-      </Card>
-    </View>
-  );
-}
-*/
-
