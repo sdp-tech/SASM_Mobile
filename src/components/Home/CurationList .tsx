498acc4f9644786afda8ed26a00c3e5fd28fdac0
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-04-11 16:16:28 +0900

    #88 feat : HomeScreen Frame except Curation Detail & PlusButton add Animation
    * #88 feat : PlusButton add Animation
    
    * #88 feat : Home Screen except CurationDetail

--- src/components/Home/CurationList .tsx
+++ src/components/Home/CurationList .tsx
@@ -1,9 +1,21 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useState } from 'react';
-import { Dimensions, FlatList, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { HomeStackParams, item } from '../../pages/Home';
 import SearchBar from '../../common/SearchBar';
 import ItemCard from './ItemCard';
+import styled from 'styled-components/native';
+import GoBack from "../../assets/img/common/GoBack.svg";
+import PlusButton from '../../common/PlusButton';
+
+const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 10px;
+  margin-bottom: 10px;
+`
 
 export default function CurationList({ navigation, route }: StackScreenProps<HomeStackParams, 'List'>): JSX.Element {
   const { width, height } = Dimensions.get('window');
@@ -16,17 +28,20 @@ export default function CurationList({ navigation, route }: StackScreenProps<Hom
         data={item}
         renderItem={({ item }) => <ItemCard style={{width: width / 2 - 10, height:height / 3, margin: 5}} data={item} onPress={() => { navigation.navigate('Detail', { id: item.id }) }} />}
         ListHeaderComponent={
-          <>
-            <TouchableOpacity onPress={navigation.goBack}><Text>&lt;</Text></TouchableOpacity>
+          <Header>
+            <TouchableOpacity onPress={navigation.goBack}>
+              <GoBack/>
+            </TouchableOpacity>
             <SearchBar
               setPage={setPage}
               search={search}
               setSearch={setSearch}
-              style={{ width: '90%', backgroundColor: '#F1F1F1' }}
+              style={{ width: '85%', backgroundColor:'#F1F1F1'}}
             />
-          </>
+          </Header>
         }
-      ></FlatList>
+      />
+      <PlusButton onPress={()=>{console.log('');}}/>
     </SafeAreaView >
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, ScrollView, Text, Dimensions, TouchableOpacity, ViewBase, Image, ImageBackground } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, Dimensions, TouchableOpacity, ViewBase, Image, ImageBackground, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import CardView from '../common/CardView';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,6 +12,7 @@ import ItemCard from '../components/Home/ItemCard';
 import PlusButton from '../common/PlusButton';
 import { useNavigation } from '@react-navigation/native';
 import { TabProps } from '../../App';
+import { Request } from '../common/requests';
 
 
 const { width, height } = Dimensions.get('screen');
@@ -27,6 +29,14 @@ const RecommendPlace = styled.TouchableOpacity`
   height: ${width / 5};
   border-radius: ${width / 10};
 `
+const TextBox = styled.View`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 8px;
+  margin-bottom: 10px;
+`
 export type HomeStackParams = {
   "Home": undefined;
   "List": undefined;
@@ -34,6 +44,27 @@ export type HomeStackParams = {
     id: number;
   }
 }
+const TextStyles = StyleSheet.create({
+  Title: {
+    // fontFamily:"Inter",
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 19,
+    widht: '70%'
+  },
+  Sub: {
+    fontWeight: "400",
+    fontSize: 10,
+    lineHeight: 12,
+    color: '#595959',
+    width: width,
+  },
+  SubBlack: {
+    fontWeight: "400",
+    fontSize: 10,
+    lineHeight: 12,
+  }
+})
 
 export default function HomeScreen(): JSX.Element {
   const HomeStack = createNativeStackNavigator<HomeStackParams>();
@@ -83,22 +114,51 @@ function CurationHome({ navigation, route }: StackScreenProps<HomeStackParams, '
   const [search, setSearch] = useState<string>("");
   // 큐레이션 페이지
   const [page, setPage] = useState<number>(1);
+  // 연관 스토리 검색
+  const [storyData, setStoryData] = useState([]);
+  const request = new Request();
+
+  const getStory = async () => {
+    const response = await request.get('/stories/story_search/',{
+      page:1, 
+      search:'비건',
+      latest: true,
+    });
+    setStoryData(response.data.data.results);
+  }
+  useEffect(()=>{
+    getStory();
+  }, [])
+
   return (
     <SafeAreaView style={{ backgroundColor: '#FFFFFF' }}>
       <ScrollView>
         <SearchBar
-          style={{ width: '80%', backgroundColor: '#F1F1F1' }}
+          style={{ width: '80%', backgroundColor: '#F1F1F1', marginBottom: 10 }}
           search={search}
           setSearch={setSearch}
           setPage={setPage}
         />
-        <ItemCard
-          data={item[0]}
-          style={{ width: width - 16, height: height * 0.4, margin: 8 }}
-          onPress={() => { navigation.navigate('Detail', { id: item[0].id }) }} />
+        <CardView
+          gap={0}
+          offset={0}
+          height={height * 0.45}
+          data={item}
+          pageWidth={width}
+          dot={true}
+          renderItem={({ item }: any) => (
+            <ItemCard
+              data={item}
+              style={{ width: width - 16, height: height * 0.4, margin: 8 }}
+              onPress={() => { navigation.navigate('Detail', { id: item.id }) }} />
+          )}
+        />
+
         <SectionCuration>
-          <Text>큐레이션</Text>
-          <TouchableOpacity onPress={() => { navigation.navigate('List') }}><Text>모두보기 &gt;</Text></TouchableOpacity>
+          <TextBox>
+            <Text style={TextStyles.Title}>큐레이션</Text>
+            <TouchableOpacity onPress={() => { navigation.navigate('List') }}><Text style={TextStyles.SubBlack}>모두보기 &gt;</Text></TouchableOpacity>
+          </TextBox>
           <CardView
             gap={16}
             offset={24}
@@ -115,8 +175,11 @@ function CurationHome({ navigation, route }: StackScreenProps<HomeStackParams, '
           />
         </SectionCuration>
         <SectionCuration>
-          <Text>이 큐레이션은 어때요?</Text>
-          <TouchableOpacity onPress={() => { navigation.navigate('List') }}><Text>모두보기 &gt;</Text></TouchableOpacity>
+          <TextBox>
+            <Text style={TextStyles.Title}>이 큐레이션은 어때요?</Text>
+            <TouchableOpacity onPress={() => { navigation.navigate('List') }}><Text style={TextStyles.SubBlack}>모두보기 &gt;</Text></TouchableOpacity>
+            <Text style={TextStyles.Sub}>유저가 직접 작성한 큐레이션</Text>
+          </TextBox>
           <ItemCard
             data={item[0]}
             style={{ width: width - 16, height: height * 0.25, margin: 8 }}
@@ -131,7 +194,9 @@ function CurationHome({ navigation, route }: StackScreenProps<HomeStackParams, '
             onPress={() => { navigation.navigate('Detail', { id: item[2].id }) }} />
         </SectionCuration>
         <SectionCuration>
-          <Text>추천 장소</Text>
+          <TextBox>
+            <Text style={TextStyles.Title}>추천 장소</Text>
+          </TextBox>
           <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
             <RecommendPlace
               onPress={() => { navigationToMap.navigate('맵', { id: 0 }) }}>
@@ -179,8 +244,24 @@ function CurationHome({ navigation, route }: StackScreenProps<HomeStackParams, '
             </RecommendPlace>
           </View>
         </SectionCuration>
-        <SectionCuration>
-
+        <SectionCuration style={{marginBottom:30}}>
+          <TextBox>
+            <Text style={TextStyles.Title}>"비건"과 관련된 스토리</Text>
+          </TextBox>
+          <CardView
+            gap={10}
+            data={storyData}
+            offset={10}
+            pageWidth={width* 0.4}
+            height={height* 0.25}
+            dot={false}
+            renderItem={({item}:any)=>(
+              <ItemCard
+                style={{ width: width * 0.4, height: height * 0.25, marginHorizontal: 5 }}
+                data={item}
+                onPress={() => { navigation.navigate('Detail', { id: item.id }) }} />
+            )}
+          ></CardView>
         </SectionCuration>
       </ScrollView>
       <PlusButton onPress={() => { console.log('clicked') }} />
