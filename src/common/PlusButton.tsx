6aa826347247ee92637ff778027b83b4536476e3
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-04-11 16:12:34 +0900

    #88 feat : PlusButton add Animation

--- src/common/PlusButton.tsx
+++ src/common/PlusButton.tsx
@@ -1,7 +1,8 @@
 import { useFocusEffect } from '@react-navigation/native';
-import React, { useCallback, useState } from 'react';
-import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, Dimensions, Pressable } from 'react-native';
 import styled from 'styled-components/native';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const ButtonWrapper = styled.TouchableOpacity`
   width: 50px;
@@ -24,17 +25,44 @@ export default function PlusButton({ onPress }: PlusProps): JSX.Element {
   //탭 전환 시 modal 자동 닫기
   useFocusEffect(useCallback(() => {
     setModalView(false);
-  }, []))
+  }, []));
+  const animatedPosition = useSharedValue(0);
+  const animatedStyle1 = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: withTiming(animatedPosition.value, { duration: 500 }) }]
+    };
+  })
+  const animatedStyle2 = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: withTiming(2 * animatedPosition.value, { duration: 500 }) }]
+    };
+  })
+  useEffect(() => {
+    if (modalView) {
+    }
+  }, [modalView]);
   return (
     <>
       {
         modalView &&
-        <View style={{ width: width, height: height, backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', right: 0, bottom: 0 }}>
-        </View>
+        <Pressable
+          onPress={() => { setModalView(!modalView); animatedPosition.value = modalView ? 0 : -60 }}
+          style={{ width: width, height: height, backgroundColor: 'rgba(0,0,0,0.5)', position: 'absolute', right: 0, bottom: 0 }}>
+        </Pressable>
       }
-      <ButtonWrapper onPress={() => { setModalView(!modalView) }}>
-        <Text>{modalView ? 'down' : 'up'}</Text>
-      </ButtonWrapper>
+      <Animated.View style={animatedStyle2}>
+        <ButtonWrapper onPress={() => { }}>
+        </ButtonWrapper>
+      </Animated.View>
+      <Animated.View style={animatedStyle1}>
+        <ButtonWrapper onPress={() => { }}>
+        </ButtonWrapper>
+      </Animated.View>
+      <Animated.View>
+        <ButtonWrapper onPress={() => { setModalView(!modalView); animatedPosition.value = modalView ? 0 : -60 }}>
+          <Text>{modalView ? 'down' : 'up'}</Text>
+        </ButtonWrapper>
+      </Animated.View>
     </>
   )
 }
