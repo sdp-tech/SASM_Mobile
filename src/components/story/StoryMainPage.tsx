95a2baff7d23aafd8a89b5d19792fdbce8ab8ade
SJ-Kwak <seojin7@ewhain.net>
2023-05-09 17:58:30 +0900

    sdp-tech#81 feat: apply new story list view

--- src/components/story/StoryMainPage.tsx
+++ src/components/story/StoryMainPage.tsx
@@ -1,172 +1,188 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
-import { SafeAreaView, Text, View, ScrollView, StyleSheet, TouchableOpacity, FlatList, Dimensions, Image, ImageBackground } from 'react-native';
-import styled from 'styled-components/native';
-import SearchBar from '../../common/SearchBar';
-import StoryList from './components/StoryList';
-import { Request } from '../../common/requests';
-import { useIsFocused, useFocusEffect } from '@react-navigation/native';
-import { StoryProps } from '../../pages/Story';
-import CardView from '../../common/CardView';
-import MainCard from './components/MainCard';
-import SwipeList from './components/SwipeList';
-import ToListView from '../../assets/img/Story/ToListView.svg';
-import DropDown from '../../common/DropDown';
-import PlusButton from '../../common/PlusButton';
+import { useState, useCallback } from "react";
+import {
+  SafeAreaView,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import SearchBar from "../../common/SearchBar";
+import { Request } from "../../common/requests";
+import { useIsFocused, useFocusEffect } from "@react-navigation/native";
+import { StoryProps } from "../../pages/Story";
+import CardView from "../../common/CardView";
+import Add from "../../assets/img/Story/Add.svg";
+import StorySearch from "./components/StorySearch";
+import Category from "../../common/Category";
+import MainCard from "./components/MainCard";
+import DropDown from "../../common/DropDown";
+import Arrow from "../../assets/img/common/Arrow.svg";
 
-const StoryMainPage = ({ navigation, route}: StoryProps) => {
+const StoryMainPage = ({ navigation, route }: StoryProps) => {
   const [item, setItem] = useState([] as any);
-  const [orderList, setOrderList] = useState<boolean>(true);
+  //const [orderList, setOrderList] = useState<boolean>(true);
   const [page, setPage] = useState<number>(1);
   const [nextPage, setNextPage] = useState<any>(null);
   const [search, setSearch] = useState<string>('');
-  const [isSearch, setIsSearch] = useState<boolean>(false);
   const [refreshing, setRefreshing] = useState<boolean>(false);
-  const [latest, setLatest] = useState<boolean>(false);
   const [checkedList, setCheckedList] = useState([] as any);
-  const [pressed, setPressed] = useState<boolean>(false);
   const [count, setCount] = useState<number>(0);
-  const { width, height } = Dimensions.get('screen');
+  const { width, height } = Dimensions.get("screen");
 
   const request = new Request();
 
-  useFocusEffect(useCallback(() => {
-    getStories();
-  }, [page, orderList]));
-
-  useEffect(() => {
-    handleSearchToggle();
-  }, [search])
+  useFocusEffect(
+    useCallback(() => {
+      handleSearchToggle();
+      getStories();
+    }, [page, search])
+  );
 
   const handleSearchToggle = async () => {
-    const response = await request.get('/stories/story_search/', {
-      page: page,
-      search: search,
-      latest: orderList
-    }, null);
-    setItem(response.data.data.results);
-    setCount(response.data.data.count);
-  }
+    if (search.length === 0) {
+      setPage(1);
+      setItem([]);
+    }
+  };
 
   const getStories = async () => {
-    const response = await request.get('/stories/story_search/', {
+    const response = await request.get("/stories/story_search/", {
       page: page,
-      search: null,
-      latest: orderList
+      search: search,
+      latest: true
     }, null);
-    setItem([...response.data.data.results]);
+
+    if (page === 1) {
+      setItem(response.data.data.results);
+    } else {
+      setItem([...item, ...response.data.data.results]);
+    }
+    setCount(response.data.data.count);
     setNextPage(response.data.data.next);
-  }
+  };
+  
   const onRefresh = async () => {
     if (!refreshing || page !== 1) {
-        setRefreshing(true);
-        setPage(1);
-        setRefreshing(false);
+      setRefreshing(true);
+      setPage(1);
+      setRefreshing(false);
     }
-}
+  };
 
-  const CATEGORY_LIST = [
-    { id: 0, data: "식당 및 카페", name: "식당·카페" },
-    { id: 1, data: "전시 및 체험공간", name: "전시·체험" },
-    { id: 2, data: "제로웨이스트 샵", name: "제로웨이스트" },
-    { id: 3, data: "도시 재생 및 친환경 건축물", name: "건축물" },
-    { id: 4, data: "복합 문화 공간", name: "복합문화" },
-    { id: 5, data: "녹색 공간", name: "녹색공간" },
-  ];
+  const onEndReached = async () => {
+    if(search.length > 0 && nextPage !== null){
+      setPage(page + 1);
+    }
+    else {
+      return;
+    }
+  };
 
-  const [selected, setSelected] = useState({ label: '조회수 순', value: 1 });
+  const [orderList, setOrderList] = useState(0);
   const toggleItems = [
-    { label: '조회수 순', value: 1 },
-    { label: '좋아요 순', value: 2 },
-    { label: '최신 순', value: 3 },
+    { label: '에디터의 추천 스토리', title: '에디터가 직접 선정한 알찬 스토리를 둘러보세요.', value: 0 },
+    { label: '방금 올라온 스토리', title: '가장 생생한 장소의 이야기가 궁금하다면?', value: 1 },
+    { label: '이번 달 인기 스토리', title: '5월, 가장 많은 사람들의 관심을 받은 이야기를 둘러보세요.', value: 2 },
   ]
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white", }}>
       <SearchBar
         setPage={setPage}
         search={search}
         setSearch={setSearch}
-        style={{ backgroundColor: '#D9D9D9', marginTop: 20 }}
+        style={{ backgroundColor: "#D9D9D9", opacity: 0.3, marginTop: 20 }}
         placeholder="장소명 / 내용 / 카테고리로 검색해보세요!"
+        placeholderTextColor={"black"}
       />
-        { search.length > 0 ? (
-          <View style={{alignItems: 'center', paddingHorizontal: 30, paddingTop: 10, flex: 1}}>
-            {/* <Category checkedList={checkedList} setCheckedList={setCheckedList} /> */}
-            <CardView 
-              gap={0} offset={0} data={CATEGORY_LIST} pageWidth={100} height={50} dot={false}
-              renderItem={({item}: any) => (
-                <TouchableOpacity style={{alignItems: 'center', marginHorizontal: 6, marginVertical: 6}} onPress = {() => {setPressed(!pressed)}}>
-                  <Text style={{alignSelf: 'flex-start', borderRadius: 12, paddingHorizontal: 16, paddingVertical: 4, overflow: 'hidden', fontSize: 12, fontWeight: '400', lineHeight: 14,
-                    backgroundColor: pressed ? '#3B3B3B' : '#F2F2F2', color: pressed ? 'white' : '#ADADAD', borderColor: '#B1B1B1', borderWidth: 1}}>
-                    {item.name}</Text>
-                </TouchableOpacity>
-              )}
-            />
-            <View style={{flexDirection: 'row'}}>
-              <Text style={{fontSize: 12, fontWeight: '600', flex: 3}}>검색결과 {count}개</Text>
-              <DropDown data={toggleItems} onSelect={setSelected}/>
+      {search.length > 0 ? (
+        <StorySearch
+          item={item}
+          //orderList={""}
+          count={count}
+          navigation={navigation}
+          checkedList={checkedList}
+          setCheckedList={setCheckedList}
+          onEndReached={onEndReached}
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      ) : (
+        <View>
+          <View style={{ flexDirection: "row", paddingHorizontal: 30, paddingTop: 30 }}>
+            <View style={{flex: 1}}>
+              <Text style={textStyles.title}>{toggleItems[orderList].label}</Text>
+              <Text style={textStyles.subtitle}>{toggleItems[orderList].title}</Text>
             </View>
-            <StoryList
-              info={item}
-              onRefresh={onRefresh}
-              refreshing={refreshing}
-              //onEndReached={onEndReached}
-              navigation={navigation}
-              search={true}
+            <TouchableOpacity onPress={() => setOrderList((orderList+1)%3)}>
+              <Arrow transform={[{rotate: '90deg'}]}/>
+            </TouchableOpacity>
+          </View>
+          <View style={{backgroundColor: "white", width: width, marginVertical: 10, shadowOffset: { width: 0, height: 1 }, shadowColor: "black", shadowOpacity: 0.1}}>
+            <Category
+              checkedList={checkedList}
+              setCheckedList={setCheckedList}
+              story={true}
             />
           </View>
-        ) : (
-          <ScrollView 
-          nestedScrollEnabled = {true}
-          contentContainerStyle = {{alignItems: 'center', padding: 30}}>
-          <View>
-            <View style={{flexDirection: 'row'}}>
-              <Text style={textStyles.title}>오늘의 인기 스토리</Text>
-              <TouchableOpacity style={{marginLeft: 180, marginTop: 5}} onPress={() => {navigation.replace('StoryList')}}><ToListView/></TouchableOpacity>
-            </View>
-            <Text style={textStyles.subtitle}>가장 많은 사람들의 관심을 받은 스토리가 궁금하다면?</Text>
-            <SwipeList item={item} navigation={navigation} />
-            <Text style = {textStyles.title}>이런 스토리는 어때요?</Text>
-            <Text style={textStyles.subtitle}>에디터가 직접 선정한 알찬 스토리를 둘러보세요.</Text>
-            <CardView 
-              gap={0} offset={0} data={CATEGORY_LIST} pageWidth={width} height={20} dot={false}
-              renderItem={({item}: any) => (
-              <TouchableOpacity style={{marginHorizontal: 6, borderBottomColor: pressed ? '#444444' : '#F2F2F2', borderBottomWidth: 1}} onPress = {() => {setPressed(!pressed)}}>
-                <Text style={{paddingHorizontal: 16, overflow: 'hidden', fontSize: 12, fontWeight: '400', lineHeight: 14, color: '#444444'}}>
-                  {item.name}</Text>
-              </TouchableOpacity>
-              )}
-            />
-            <View style={{marginTop: 20}}>
-              <SwipeList item={item} navigation={navigation} />
-            </View>
+          <View style={{paddingVertical: 20}}>
+            <CardView data={item} gap={0} offset={0} pageWidth={width} height={width*0.84+150} dot={true}
+              renderItem={({item}: any) => {
+                return (
+                  <MainCard
+                    id={item.id}
+                    rep_pic={item.rep_pic}
+                    place_name={item.place_name}
+                    title={item.title}
+                    story_like={item.story_like}
+                    category={item.category}
+                    preview={item.preview}
+                    writer={item.writer}
+                    nickname={item.nickname}
+                    writer_is_verified={item.writer_is_verified}
+                    navigation={navigation}
+                    width={width}
+                  />
+                )
+              }}/>
           </View>
-          </ScrollView>
-        )}
-        { search.length > 0 ? (
-                <></>
-            ) : (
-                <PlusButton onPress={() => { console.log('clicked') }} />
-                // <TouchableOpacity onPress={()=>{navigation.navigate('WriteStory')}}
-                //     style={{position: 'absolute', marginTop: height*0.8, marginLeft: width*0.85, width: 45, height: 45, borderRadius: 60, alignItems: 'center', justifyContent: 'center', backgroundColor: '#75E59B'}}>
-                //     <Add />
-                // </TouchableOpacity>
-            )}
+          <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("WriteStory");
+          }}
+          style={{
+            position: "absolute",
+            width: 45,
+            height: 45,
+            top: height * 0.7,
+            left: width * 0.85,
+            borderRadius: 60,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#209DF5",
+            opacity: 0.7
+          }}
+        >
+          <Add />
+        </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const textStyles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: '700'
+    fontWeight: "700",
   },
   subtitle: {
     fontSize: 10,
-    fontWeight: '400',
+    fontWeight: "400",
     marginTop: 2,
-    marginBottom: 15,
+    marginBottom: 5,
   },
-})
+});
 
 export default StoryMainPage;
