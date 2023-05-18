099a1ed24dd9675abe39f61ae59cdbcda21d58ba
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2023-05-18 18:41:12 +0900

    #89 feat: make mypick(place,story)screen roughly
    * #89 feat: mypick(myplace,mystory)screen roughly

--- src/components/mypick/myplace/ItemCard.jsx
+++ src/components/mypick/myplace/ItemCard.jsx
@@ -10,11 +10,20 @@ import { MatchCategory, CATEGORY_LIST } from "../../../common/Category";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 
+
 const styles = StyleSheet.create({
+  Container: {
+    width: 120,
+    marginTop: 3,
+    marginBottom: 3,
+    paddingVertical: 3,
+    marginHorizontal:3,
+    //backgroundColor:'red'
+  },
   ImageSection: {
     alignItems: 'center',
     //flex:1
-    marginBottom: 20
+    marginBottom: 5
   },
   NameHeartCategoryBox: {
     width: '100%',
@@ -29,20 +38,7 @@ const styles = StyleSheet.create({
   },
   NameBox: {
     //backgroundColor: 'white'
-  },
-  CategoryIcon: {
-    //display: 'flex',
-    backgroundColor: 'blue',
-    alignItems:'center'
-  },
-  LikeButton: {
-    border: 'none',
-    //flex:1,
-    height: 30,
-    width: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-    //backgroundColor: 'green'
+    width:'100%'
   },
 })
 
@@ -55,9 +51,9 @@ export default function ItemCard(props) {
   const navigation = useNavigation();
   const request = new Request();
   const categoryIndex = MatchCategory(props.category);
-  const categorySrc = '../../../assets/img/Category/Category'+categoryIndex+'.svg'
+  const categorySrc = '../../../assets/img/Category/Category' + categoryIndex + '.svg'
   // console.log("catgory", categoryIndex);
-  
+
 
   // 좋아요 클릭 이벤트
   const toggleLike = async () => {
@@ -68,37 +64,19 @@ export default function ItemCard(props) {
   };
 
   return (
-    <View style={{ width: 300, marginTop: 3, marginBottom:3, paddingVertical:3 }}>
+    <View style={styles.Container}>
       <View style={styles.ImageSection}>
         <Image
           source={{ uri: props.rep_pic }}
-          width={300}
+          width={120}
           height={150}
-          borderRadius={20}
         />
       </View>
       <TouchableOpacity
         onPress={() => navigation.navigate('맵', { id: props.place_id })}
-        style={{ textDecoration: 'none' }}
       >
-        <View style={styles.NameHeartCategoryBox}>
-          <View style={styles.NameBox}>
-            <Text style={{ fontSize: 18, fontWeight: 600, }}>{props.place_name}</Text>
-          </View>
-          <View style={styles.CategoryIcon}>
-            {/* <Image 
-            source={categorySrc} 
-            width={30}
-            height={30}
-            /> */}
-          </View>
-          <View style={styles.LikeButton}>
-            {props.place_like === 'ok' ? (
-              <Heart like={!like} onPress={toggleLike} />
-            ) : (
-              <Heart like={like} onPress={toggleLike} />
-            )}
-          </View>
+        <View style={styles.NameBox}>
+          <Text style={{ fontSize: 15, fontWeight: 200, }}>{props.place_name}</Text>
         </View>
       </TouchableOpacity>
     </View>
