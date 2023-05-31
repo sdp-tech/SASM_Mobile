db59452f1bdb76fce8b4032ce1321c54700caad4
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-05-31 20:26:33 +0900

    #120 design : CustomText style type change to any

--- src/common/CustomText.tsx
+++ src/common/CustomText.tsx
@@ -1,12 +1,17 @@
 import React, { ReactElement, ReactNode } from 'react';
 import { Text, TextProps, TextStyle } from 'react-native';
+import styled from 'styled-components/native';
+
+const StyledText = styled.Text`
+  font-family: Pretendard Variable;
+`
 
 interface CustomTextProps extends TextProps {
-  style?: TextStyle
+  style?: any;
 }
 
 export function TextPretendard({children, style, ...rest}:CustomTextProps):JSX.Element {
   return (
-    <Text style={{fontFamily:'Pretendard Variable', ...style}} {...rest}>{children}</Text>
+    <StyledText style={style} {...rest}>{children}</StyledText>
   )
 }
