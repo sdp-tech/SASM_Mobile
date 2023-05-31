da391b42df13ba5263edee6019cbc9414265894d
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-05-31 20:05:29 +0900

    #120 design : Custom Text props add TextStyl

--- src/common/CustomText.tsx
+++ src/common/CustomText.tsx
@@ -2,10 +2,11 @@ import React, { ReactElement, ReactNode } from 'react';
 import { Text, TextProps, TextStyle } from 'react-native';
 
 interface CustomTextProps extends TextProps {
+  style?: TextStyle
 }
 
-export function TextPretendard({children, ...rest}:CustomTextProps):JSX.Element {
+export function TextPretendard({children, style, ...rest}:CustomTextProps):JSX.Element {
   return (
-    <Text style={{fontFamily:'Pretendard Variable'}} {...rest}>{children}</Text>
+    <Text style={{fontFamily:'Pretendard Variable', ...style}} {...rest}>{children}</Text>
   )
 }
