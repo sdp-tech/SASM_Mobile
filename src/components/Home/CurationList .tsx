f9a63dd3a350aabf3336b74d3e9e3c4897fa9267
YoonDH <114418121+dgfh0450@users.noreply.github.com>
2023-07-26 21:13:26 +0900

    #96 design : replace touchable to  withoutfeedback

--- src/components/Home/CurationList .tsx
+++ src/components/Home/CurationList .tsx
@@ -5,7 +5,7 @@ import { TextPretendard as Text } from '../../common/CustomText';
 import { FlatList } from 'react-native-gesture-handler';
 import { HomeStackParams } from '../../pages/Home';
 import SearchBar from '../../common/SearchBar';
-import { SearchItemCard } from './ItemCard';
+import { SearchItemCard } from './CurationItemCard'
 import styled from 'styled-components/native';
 import Arrow from "../../assets/img/common/Arrow.svg";
 import PlusButton from '../../common/PlusButton';
