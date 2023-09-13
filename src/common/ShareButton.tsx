a488f8a2ffee68530380b91fda6609d967d306b0
SJ-Kwak <seojin7@ewhain.net>
2023-09-13 16:30:49 +0900

    sdp-tech#112 feat: add kakao share library

--- src/common/ShareButton.tsx
+++ src/common/ShareButton.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Share, Alert, TouchableOpacity } from 'react-native';
 import ShareIcon from '../assets/img/common/Share.svg';
+import KakaoShareLink from "react-native-kakao-share-link";
 
 interface ShareButtonProps {
     message: string;
+    image: string;
+    description: string;
     color: 'black' | 'white';
+    id: Number;
 }
 
 // TODO: 카카오톡 공유하기 등 기능 확장 필요
-export default function ShareButton({ message, color }: ShareButtonProps): JSX.Element {
-    const getPreview = (message: string) => {
-        return message
+export default function ShareButton({ message, color, image, description, id }: ShareButtonProps): JSX.Element {
+    const getPreview = (description: string) => {
+        return description
             .replace(/<.*?>/g, ' ') // 그외 html 태그 치환
             .replace(/\s{2,}/g, ' ') // space 두개인 경우 하나로 치환
             .substring(0, 100) // 50자만 잘라서 반환
+        
     }
-    const onShare = async () => {
+
+    const onShare = useCallback(async () => {
         try {
-            await Share.share({ message: getPreview(message) });
-        } catch (error: any) {
-            Alert.alert('공유하기가 실패하였습니다.');
+            const response = await KakaoShareLink.sendFeed({
+                content: {
+                    title: message,
+                    imageUrl: image,
+                    description: getPreview(description),
+                    link: {
+                        webUrl: "https://www.sasm.co.kr/",
+                        mobileWebUrl: "https://www.sasm.co.kr/",
+                    },
+                },
+                buttons: [
+                    {
+                        title: "SASM 앱에서 확인해보세요!",
+                        link: {
+                            androidExecutionParams: [
+                                { key: "id", value: id.toString() },
+                            ],
+                            iosExecutionParams: [
+                                { key: "id", value: id.toString() },
+                            ],
+                        },
+                    },
+                ],
+            });
+            console.log(response);
+        } catch (e: any) {
+            console.error(e);
+            Alert.alert('공유하기가 실패하였습니다.')
         }
-    };
+    }, []);
+
 
     return (
         <TouchableOpacity style={{ marginLeft: 5, marginRight: 5 }} onPress={onShare}>
